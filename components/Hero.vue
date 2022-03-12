c0c3f17d6ddb7ca9ab31c2396338e2a297aed1ed
maciejoliwa <maciejoliwa0906@gmail.com>
2022-03-12 17:48:31 +0100

    fixed another typo

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,7 +2,7 @@
   <section class="hero flex w-full h-screen p-4 flex-col items-center justify-center">
       <h1 class="text-4xl md:text-6xl py-2 px-8 text-center">Maciej Oliwa</h1>
       <p class="mt-2 text-xl md:text-2xl max-w-3xl text-center">
-          I'm a young and ambitious developer based in <b>Poland, Kraków.</b> I'm mainly skilled in <b>JavaScript</b> and <b>TypeScript</b> with <b>Node.js.</b> I'm also learning 
+          I'm a young and ambitious developer based in <b>Kraków, Poland.</b> I'm mainly skilled in <b>JavaScript</b> and <b>TypeScript</b> with <b>Node.js.</b> I'm also learning 
           <b>C</b> in order to compile it to <b>WebAssembly</b> and use it on the web.
       </p>
       <projects :prop_projects="projects"/>
